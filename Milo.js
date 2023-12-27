0f4e552dc66e219806d51af2e3c05bc3b26c573b
Manoj SInnur <manoj19.sinnur@gmail.com>
2023-12-28 00:20:59 +0530

    Download from Discord Working

--- Milo.js
+++ Milo.js
@@ -1,4 +1,5 @@
 const { Client, GatewayIntentBits, Events , WebhookClient } = require('discord.js');
+const https = require('https');
 
 class Milo{
     constructor(token){
@@ -27,7 +28,48 @@ class Milo{
         return message.id;
       }
 
+  async fetchAttachment(guildId, channelId,messageIds){
+    const attachmentUrls = [];
+    const guild = await this.client.guilds.fetch(guildId);
+    const channel = await guild.channels.fetch(channelId);
+    for (const messageId of messageIds) {
+      const message = await channel.messages.fetch(messageId);
+      const url = await message.attachments.first().url; 
+      attachmentUrls.push(url);     
+    }
+    return attachmentUrls;
+  }
+
+
+  async downloadAttachment(attachmentUrls){
+    
+    let promises = [];
+    let urls = await attachmentUrls;
+    for( const url of urls ){
+        promises.push(new Promise((resolve, reject)=>{
+            let data = [];
+            https.get(`${url}`, res => {
+                res.on("data", chunk =>{
+                   data.push(chunk);         
+                })
+                .on("end", () =>{
+                   resolve(Buffer.concat(data))
+                })
+                .on("error", err =>{
+                   console.error("Error: ", err.message)
+                   reject(err)
+                })
+            })
+        }));
+    }
+    let results = await Promise.all(promises);
+    return Buffer.concat(results);
+ }
+ 
+
+
 }
+
 
 module.exports = Milo;
 
--- app.js
+++ app.js
@@ -7,13 +7,18 @@ const fs = require('fs');
 const status = require("express-status-monitor");
 app.use(status())
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 app.use(express.urlencoded({extended:true}));
 
 app.use(express.static("public"))
 
 const { password } = require('./config.json');
+
+const Milo = require("./Milo");
+
+const { token } = require('./config.json');
+const milo =new Milo(token);
 
 const multer = require("multer");
 
@@ -57,7 +62,7 @@ const File = mongoose.model("File", fileSchema);
 
 // // Require the necessary discord.js classes
 const { Client, GatewayIntentBits, Events , WebhookClient } = require('discord.js');
-const { token } = require('./config.json');
+// const { token } = require('./config.json');
 
 const channelId = "1187095180009025598";
 const guildId = "1187095179497328711"
@@ -78,275 +83,26 @@ client.once(Events.ClientReady, readyClient => {
 });
 
 
-client.login(token);
+//Download Code:
 
-// client.on('messageCreate', async (message) => {
-//     const channel = message.channel;
-//     const permissions = channel ? channel.permissionsFor(message.guild.me) : null;
- 
-//     if (!permissions || !permissions.has('SEND_MESSAGES')) {
-//         console.log('Bot does not have permission to send messages in this channel.');
-//         return;
-//     }
- 
-//     // Bot has permission to send messages, so proceed with the action
-//     channel.send('Hello!');
-//  });
+let attachmentUrl = milo.fetchAttachment(guildId, channelId, ["1189278489199784007", "1189278527846101033"]);
 
-// client.guilds.fetch("1187095179497328711").then((guild)=>{
-//     console.log(guild.channels.fetch("1187095180009025598").then(channel=>{
-//         channel.send("hello")
-//     }));
-// })
-
-// const channel = client.guilds.fetch("1187095179497328711").channels;
-// console.log(channel)
-//  console.log(client.guilds.fetch("1187095180009025598"))
-
-// const channelID = '1187095180009025598';
-// client.channels.cache.get(channelID)
-//  .then(channel => {
-//    if(channel) {
-//        channel.send("hello");
-//    } else {
-//        console.log('Channel not found');
-//    }
-//  })
-//  .catch(console.error);
-// console.log(client.guilds.fetch("1187095179497328711"));
+attachmentUrl.then(attachmentUrl =>{
+    console.log(attachmentUrl)
+})
 
 
-
-// cha.send('hello!')
-//   .then(message => console.log(`Sent message: ${message.content}`))
-//   .catch(console.error);
-
-// const readable = fs.createReadStream("./32mb.mp4", {highWaterMark: 25 * 1024 * 1024});
-
-
-// readable.on("data", (chunk)=>{
-//     client.guilds.fetch("1187095179497328711").then((guild)=>{
-//         console.log(guild.channels.fetch("1187095180009025598").then(channel=>{
-//             channel.send({
-//                 files: [{
-//                   attachment: chunk ,
-//                   name: 'file.txt',
-//                   description: 'A description of the file'
-//                 }]
-//               }).then(message => {
-//                 console.log(`Sent message: ${message.id}`);
-//                 const file = new File({
-//                     name:"file.txt",
-//                     messageId: `${message.id}`  
-//                 })
-//                 file.save()
-//             })
-//         }));
-//     })
-// })
-
-const https = require('https');
-const { type } = require("os");
-const { log } = require("console");
-
-// File.find({name: "file.txt"}).exec()
-// .then(result=>{
-    // let msgID = result[0].messageId;
-    // let msgID = ["1188547809121677312", "1188547835143135242"]
-    // console.log(msgID);
-    // msgID.forEach(id => {
-    //     client.guilds.fetch("1187095179497328711")
-    //     .then((guild) => {
-    //        return guild.channels.fetch("1187095180009025598")
-    //        .then(channel => {
-    //            return channel.messages.fetch(`${id}`)
-    //            .then((message) => {
-    //                return message.attachments.first().url;
-    //            })
-    //        })
-    //     })
-    //     .then((attachmentUrl) => {
-    //        let id = attachmentUrl;
-    //        return new Promise((resolve, reject) => {
-    //            https.get(`${id}`, res => {
-    //                let data = [];
-    //                res.on('data', chunk => {
-    //                    data.push(chunk);
-    //                }).on('end', () => {
-    //                    console.log("completed 1 chunk download");
-    //                    resolve(Buffer.concat(data));
-    //                }).on('error', err => {
-    //                    console.error("Error: " + err.message);
-    //                    reject(err);
-    //                });
-    //            });
-    //        });
-    //     })
-    //     .then((data) => {
-    //        // Write the data to a file
-    //        fs.writeFile('output.txt', data, err => {
-    //            if (err) {
-    //                console.error("Error writing file: " + err.message);
-    //            } else {
-    //                console.log("Successfully wrote file.");
-    //            }
-    //        });
-    //     })
-    //     .catch(console.error);
-            
-        
-        
-    // });
-// } )
-
-// (async ()=>{
-//     // let result = await File.find({name: "file.txt"}).exec();
-
-//     let msgID = [ '1188406134005039125' ];
-
-//     // let msgID = result[0].messageId;
-
-//     let data = [];
-//     await msgID.forEach(id => {
-//         https.get(`https://cdn.discordapp.com/attachments/1187095180009025598/${id}/file.txt`, res => {
-        
-//         res.on('data', chunk => {
-//             data.push(chunk);
-//         }).on('end', () => {
-//             //    let buffer = Buffer.concat(data);
-//             console.log("completed 1 chunk dowload");
-
-//         });
-//         }).on('error', err => {
-//         console.error("Error: " + err.message);
-//         });       
-        
-//     });
-
-//     console.log(data)
-//     let blob= new Blob(data, {type: "octet-stream"});
-//     console.log(URL.createObjectURL(blob));
-
-    
-
-// })()
-// (async ()=>{
-//     let msgID = [ '1188406133631754250' ];
- 
-//     let dataPromises = msgID.map(id => {
-//         return new Promise((resolve, reject) => {
-//             https.get(`https://cdn.discordapp.com/attachments/1187095180009025598/${id}/file.txt`, res => {
-//                 let data = [];
-//                 res.on('data', chunk => {
-//                    data.push(chunk);
-//                 }).on('end', () => {
-//                    console.log("completed 1 chunk download");
-//                    resolve(data);
-//                 }).on('error', err => {
-//                    console.error("Error: " + err.message);
-//                    reject(err);
-//                 });
-//             });
-//         });
-//     });
- 
-//     let data = await Promise.all(dataPromises);
- 
-//     let fullData = Buffer.concat(data[0]);
-
-//     // Write the data to a file
-//     fs.writeFile('output.txt', fullData, err => {
-//         if (err) {
-//             console.error("Error writing file: " + err.message);
-//         } else {
-//             console.log("Successfully wrote file.");
-//         }
-//     });
-//  })()
-
-// const fs = require('fs');
-// const https = require('https');
-
-// client.guilds.fetch("1187095179497328711")
-// .then((guild) => {
-//    return guild.channels.fetch("1187095180009025598")
-//    .then(channel => {
-//        return channel.messages.fetch('1188117394388963348')
-//        .then((message) => {
-//            return message.attachments.first().url;
-//        })
-//    })
-// })
-// .then((attachmentUrl) => {
-//    let id = attachmentUrl;
-//    return new Promise((resolve, reject) => {
-//        https.get(`${id}`, res => {
-//            let data = [];
-//            res.on('data', chunk => {
-//                data.push(chunk);
-//            }).on('end', () => {
-//                console.log("completed 1 chunk download");
-//                resolve(Buffer.concat(data));
-//            }).on('error', err => {
-//                console.error("Error: " + err.message);
-//                reject(err);
-//            });
-//        });
-//    });
-// })
-// .then((data) => {
-//    // Write the data to a file
-//    fs.writeFile('output.txt', data, err => {
-//        if (err) {
-//            console.error("Error writing file: " + err.message);
-//        } else {
-//            console.log("Successfully wrote file.");
-//        }
-//    });
-// })
-// .catch(console.error);
-
- 
-
-// (async ()=>{
-//     let result = await File.find({name: "file.txt"}).exec();
- 
-//     let msgID = result[0].messageId;
- 
-//     let dataPromises = msgID.map(id => {
-//         return new Promise((resolve, reject) => {
-//             https.get(`https://cdn.discordapp.com/attachments/1187095180009025598/${id}/file.txt`, res => {
-//                 let data = [];
-//                 res.on('data', chunk => {
-//                    data.push(chunk);
-//                 }).on('end', () => {
-//                    resolve(Buffer.concat(data));
-//                 }).on('error', err => {
-//                    reject(err);
-//                 });
-//             });
-//         });
-//     });
- 
-//     let data = await Promise.all(dataPromises);
-//     console.log(data);
-//     let blob= new Blob(data);
-//     console.log(URL.createObjectURL(blob));
-//  })()
-
- 
-
-
-
-// client.guilds.fetch("1187095179497328711").then((guild)=>{
-//     guild.channels.fetch("1187095180009025598").then(channel=>{
-//         console.log(channel.messages.fetch('1188117394388963348').then((message )=> {
-//             console.log("Message Retreived:",message.attachments.first().url)})
-//         .catch(console.error))
-//     });
-// })
-
-
+milo.downloadAttachment(attachmentUrl).then((data) => {
+    // Write the data to a file
+    fs.writeFile('output.txt', data, err => {
+        if (err) {
+            console.error("Error writing file: " + err.message);
+        } else {
+            console.log("Successfully wrote file.");
+        }
+    });
+ })
+ .catch(console.error);
 
 
 
@@ -368,9 +124,6 @@ app.post("/", upload.single("myFile"), (req, res)=>{
 //   path: 'uploads\\1703500677726- ScreenShot-2023-1-26_14-47-11.png',
 //   size: 3022143
 // }
-const Milo = require("./Milo");
-
-    const milo =new Milo(token);
     const fileSize = req.file.size;
     const fileName = req.file.originalname;
     const path = req.file.path;
@@ -398,38 +151,7 @@ const Milo = require("./Milo");
          file.save();
 
         } 
-    })
-    
-
-
-//     let messageIds = [];
-
-// readable.on("data", (chunk) => {
-//   client.guilds.fetch("1187095179497328711").then((guild) => {
-//       guild.channels.fetch("1187095180009025598").then(async channel => {
-//           const message = await channel.send({
-//               files: [{
-//                 attachment: Blob[chunk],
-//                 name: `${fileName}.txt`,
-//                 description: 'A description of the file'
-//               }]
-//           });
-
-//           console.log(`Sent message: ${message.id}`);
-//           messageIds.push(message.id);
-//       });
-//   });
-// });
-
-// // After all chunks have been processed, save the file to the database
-// const file = new File({
-//    name: `${fileName}`,
-//    messageIds: messageIds
-// });
-// file.save();
-
-
-
+    }) 
 })
 
 
